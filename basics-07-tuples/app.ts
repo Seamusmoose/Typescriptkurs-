b4062883f49d36660c88482e0e034b923fb1fc83
Seamus O'Sullivan <info@seamuso.com>
2023-01-12 16:29:47 +0100

    compiler explanation

--- basics-07-tuples/app.ts
+++ basics-07-tuples/app.ts
@@ -2,20 +2,19 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+
+
+enum Role {Admin, ReadOnly, Author}
+
+const person = {
   name: 'Maximilian',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.Admin
 };
 
- person.role.push('admin');
-// person.role[1] = 10;
+ //person.role.push('admin');
+/// person.role[1] = 10;
 
 // person.role = [0, 'admin', 'user'];
 
